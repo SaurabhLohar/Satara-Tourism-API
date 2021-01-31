9eb07d94f7f66404d2e8fd5684447e18bb3eba2b
Saurabh Lohar <61544729+SaurabhLohar@users.noreply.github.com>
2021-01-31 19:46:38 +0530

    some issue

--- core/settings.py
+++ core/settings.py
@@ -40,16 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    
 
     'tourism_API',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
